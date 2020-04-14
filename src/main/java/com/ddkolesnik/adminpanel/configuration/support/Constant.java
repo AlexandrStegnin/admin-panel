c40a51314f858f5958280791da9895f37c62fbb7
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-04-14 11:16:53 +0500

    Добавили класс для хранения константных значений

---
+++ src/main/java/com/ddkolesnik/adminpanel/configuration/support/Constant.java
@@ -0,0 +1,18 @@
+package com.ddkolesnik.adminpanel.configuration.support;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+public class Constant {
+
+    //    App roles constants
+    public static final String ROLE_PREFIX = "ROLE_";
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+    public static final String ROLE_ADMIN = ROLE_PREFIX + ADMIN;
+    public static final String ROLE_USER = USER;
+
+    public static final String APP_TOKEN_PAGE = "app/tokens";
+
+}
