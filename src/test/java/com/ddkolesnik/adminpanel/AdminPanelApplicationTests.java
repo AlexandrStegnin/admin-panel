ae38ca32b93f75cba9de99b135def04944d17d10
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-04-14 10:00:39 +0500

    Инициализация проекта

---
+++ src/test/java/com/ddkolesnik/adminpanel/AdminPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddkolesnik.adminpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPanelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
