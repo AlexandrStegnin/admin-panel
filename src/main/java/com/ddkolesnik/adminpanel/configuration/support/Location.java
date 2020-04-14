3175d3f7fd925071a96a6375aaca46dfe52850d2
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-04-14 11:16:33 +0500

    Добавили файл для хранения путей приложения

---
+++ src/main/java/com/ddkolesnik/adminpanel/configuration/support/Location.java
@@ -0,0 +1,13 @@
+package com.ddkolesnik.adminpanel.configuration.support;
+
+/**
+ * Файл для хранения путей приложения
+ *
+ * @author Alexandr Stegnin
+ */
+
+public class Location {
+
+    public static final String LOGIN_PAGE = "login";
+
+}
