bea88ef595d3f541c87a9e4f38dc906c3144fe1d
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-04-20 13:33:48 +0500

    Добавлены команды для управления пользователями.

---
+++ src/main/java/com/ddkolesnik/adminpanel/command/user/CreateUserCommand.java
@@ -0,0 +1,25 @@
+package com.ddkolesnik.adminpanel.command.user;
+
+import com.ddkolesnik.adminpanel.command.Command;
+import com.ddkolesnik.adminpanel.model.User;
+import com.ddkolesnik.adminpanel.service.UserService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class CreateUserCommand implements Command {
+
+    User user;
+    UserService userService;
+
+    @Override
+    public void execute() {
+        userService.create(user);
+    }
+}
---
+++ src/main/java/com/ddkolesnik/adminpanel/command/user/UpdateUserCommand.java
@@ -0,0 +1,26 @@
+package com.ddkolesnik.adminpanel.command.user;
+
+
+import com.ddkolesnik.adminpanel.command.Command;
+import com.ddkolesnik.adminpanel.model.User;
+import com.ddkolesnik.adminpanel.service.UserService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * @author Alexandr Stegnin
+ */
+
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class UpdateUserCommand implements Command {
+
+    User user;
+    UserService userService;
+
+    @Override
+    public void execute() {
+        userService.update(user);
+    }
+}
