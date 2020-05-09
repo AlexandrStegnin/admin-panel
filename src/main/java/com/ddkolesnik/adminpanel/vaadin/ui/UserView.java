9ac06dff7dc6711f1b81bd2c392c229657ba1dfa
Alexandr Stegnin <alexandr.stegnin@mail.ru>
2020-05-09 20:38:07 +0500

    Добавлен фильтр по роли. Стилизация.

--- src/main/java/com/ddkolesnik/adminpanel/vaadin/ui/UserView.java
+++ src/main/java/com/ddkolesnik/adminpanel/vaadin/ui/UserView.java
@@ -9,10 +9,12 @@ import com.ddkolesnik.adminpanel.vaadin.custom.CustomAppLayout;
 import com.ddkolesnik.adminpanel.vaadin.form.UserForm;
 import com.ddkolesnik.adminpanel.vaadin.support.VaadinViewUtils;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.router.Route;
@@ -41,6 +43,8 @@ public class UserView extends CustomAppLayout {
 
     private UserForm userForm;
 
+    private final ComboBox<Role> roleComboBox;
+
     public UserView(UserService userService, RoleService roleService) {
         super(userService);
         this.userService = userService;
@@ -49,6 +53,8 @@ public class UserView extends CustomAppLayout {
         this.dataProvider = new ListDataProvider<>(getAllUsers()); // инициализация провайдера с вставкой в него данных
         this.addNewBtn = VaadinViewUtils.createButton(
                 "СОЗДАТЬ ПОЛЬЗОВАТЕЛЯ", "add", "submit", "8px 13px 8px 13px");
+        this.roleComboBox = new ComboBox<>("Фильтр: ");
+
         init(); // инициализируем форму
     }
 
@@ -100,12 +106,33 @@ public class UserView extends CustomAppLayout {
         grid.getColumns()
                 .forEach(column -> column.setAutoWidth(true));
         grid.setClassName("my-grid");
+
+        roleComboBox.setItems(roleService.findAll());
+        roleComboBox.addValueChangeListener(event -> applyFilter(dataProvider));
         /* вертикальный слой, на котором размещаем кнопку и под ней Grid */
-        VerticalLayout verticalLayout = new VerticalLayout(addNewBtn, grid);
+
+        roleComboBox.setItemLabelGenerator(Role::getHumanized);
+        roleComboBox.getStyle().set("width", "200px");
+        roleComboBox.setClearButtonVisible(true);
+        roleComboBox.getElement().setAttribute("theme", "align-center");
+        addNewBtn.getStyle().set("margin-left", "auto");
+        HorizontalLayout buttonsLayout = new HorizontalLayout(roleComboBox, addNewBtn);
+        buttonsLayout.setSizeFull();
+        buttonsLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        buttonsLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
+        VerticalLayout verticalLayout = new VerticalLayout(buttonsLayout, grid);
         verticalLayout.setAlignItems(FlexComponent.Alignment.END);
 
         // устанавливаем нашей странице контент в виде вертикального слоя, созданного выше
         setContent(verticalLayout);
+    }
+
+    private void applyFilter(ListDataProvider<User> dataProvider) {
+        dataProvider.clearFilters();
+        if (roleComboBox.getValue() != null) {
+            dataProvider.addFilter(user -> user.getRoles().contains(roleComboBox.getValue()));
+        }
     }
 
     private List<User> getAllUsers() {
